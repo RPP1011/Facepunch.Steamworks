074b88474a4b39b8b9229f8593ffa994f5c50516
Ricky <ricky@rpgrafx.com>
2024-06-09 23:20:10 -0700

    Remov references to Task

--- Facepunch.Steamworks/Generated/CustomEnums.cs
+++ Facepunch.Steamworks/Generated/CustomEnums.cs
@@ -2,7 +2,6 @@ using System;
 using System.Runtime.InteropServices;
 using System.Linq;
 using Steamworks.Data;
-using System.Threading.Tasks;
 
 namespace Steamworks
 {
--- Facepunch.Steamworks/ServerList/Favourites.cs
+++ Facepunch.Steamworks/ServerList/Favourites.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Steamworks.ServerList
 {
